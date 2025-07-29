ac4ecbb4176b71ca0aed74beedcdae6b255c7ede
Julio Barba <juliobbv@gmail.com>
2025-07-29 04:49:02 -0400

    Introduce alt lambda factors

--- Source/Lib/Codec/pcs.h
+++ Source/Lib/Codec/pcs.h
@@ -1246,6 +1246,7 @@ typedef struct PictureControlSetInitData {
     int     hbd_mds;
     uint8_t noise_norm_strength;
     uint8_t kf_tf_strength;
+    uint8_t alt_lambda_factors;
 } PictureControlSetInitData;
 
 /**************************************
