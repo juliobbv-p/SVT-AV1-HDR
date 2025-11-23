0e7fc18d8d68280b65be2daecc753a3a94b16e11
Julio Barba <juliobbv@gmail.com>
2025-11-22 19:29:42 -0500

    Introduce light RDOQ

--- Source/Lib/Codec/full_loop.c
+++ Source/Lib/Codec/full_loop.c
@@ -1024,7 +1024,8 @@ static void svt_av1_optimize_b(PictureControlSet* pcs, ModeDecisionContext* ctx,
                                int16_t dc_sign_context, const TranLow* coeff_ptr, const MacroblockPlane* p,
                                TranLow* qcoeff_ptr, TranLow* dqcoeff_ptr, uint16_t* eob, const QuantParam* qparam,
                                TxSize tx_size, TxType tx_type, bool is_inter, uint8_t use_sharpness,
-                               uint8_t delta_q_present, uint8_t picture_qp, uint32_t lambda, int plane) {
+                               uint8_t delta_q_present, uint8_t picture_qp, uint32_t lambda, int plane,
+                               bool light_rdoq) {
     int                    sharpness  = 0; // No Sharpness
     const ScanOrder* const scan_order = get_scan_order(tx_size, tx_type);
     const int16_t*         scan       = scan_order->scan;
@@ -1053,6 +1054,11 @@ static void svt_av1_optimize_b(PictureControlSet* pcs, ModeDecisionContext* ctx,
             rweight   = 0;
         }
     }
+
+    if (light_rdoq) {
+        rweight = 10;
+    }
+
     const int64_t  rdmult = (((((int64_t)lambda * plane_rd_mult[is_inter][plane_type]) * rweight) / 100) + 2) >> rshift;
     uint8_t* const levels = set_levels(ctx->md_levels_buf, width, height);
 
@@ -1727,6 +1733,21 @@ uint8_t svt_aom_quantize_inv_quantize(PictureControlSet* pcs, ModeDecisionContex
             }
         }
     }
+
+    bool light_rdoq = false;
+
+    // have rdoq be lightly performed for blocks with very low dc saturation under certain conditions
+    // to prevent cases of color blotching due to aggressive coefficient decimation/reduction
+    if (is_encode_pass && tx_type != IDTX && component_type != COMPONENT_LUMA && *eob > 0) {
+        const TranLow dc = quant_coeff[0];
+
+        if (dc >= -1 && dc <= 1 && *eob <= (n_coeffs / 16)) {
+            light_rdoq = true;
+        } else if (dc >= -4 && dc <= 4 && *eob <= 1) {
+            light_rdoq = true;
+        }
+    }
+
     if (perform_rdoq && *eob != 0) {
         // Perform rdoq
         svt_av1_optimize_b(pcs,
@@ -1746,7 +1767,8 @@ uint8_t svt_aom_quantize_inv_quantize(PictureControlSet* pcs, ModeDecisionContex
                            pcs->ppcs->frm_hdr.delta_q_params.delta_q_present,
                            pcs->ppcs->picture_qp,
                            lambda,
-                           (component_type == COMPONENT_LUMA) ? 0 : 1);
+                           (component_type == COMPONENT_LUMA) ? 0 : 1,
+                           light_rdoq);
     }
 
     // Apply coefficient shaving for luma after all quantization/RDOQ is complete.
